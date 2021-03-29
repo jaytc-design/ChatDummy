3efc4f6ac99215c2834c568854e1a21cfe07d926
jaytc-design <johnthomascanton@gmail.com>
2021-03-29 18:34:06 -0400

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,2 +1,10 @@
 local clientFunctions = {}
 local serverFunctions = {}
+
+
+
+if game:GetService("RunService"):IsServer() then
+    return serverFunctions
+elseif game:GetService("RunService"):IsClient() then
+    return clientFunctions
+end
