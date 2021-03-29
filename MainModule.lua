f40e3a5116634f353cb4c732080fec2928d492d2
jaytc-design <johnthomascanton@gmail.com>
2021-03-29 18:36:14 -0400

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,7 +1,12 @@
 local clientFunctions = {}
 local serverFunctions = {}
 
-
+function clientFunctions:ChatMakeSystemMessage(...)
+    local succ,err = pcall(Function()
+        game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage", {...})
+    end)
+    return succ,err
+end
 
 if game:GetService("RunService"):IsServer() then
     return serverFunctions
